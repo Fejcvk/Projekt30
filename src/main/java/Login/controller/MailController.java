dc74737d2f8e24c9ab99c51207b834e9cb5d4134
Karol Talarek <karol@puchaczy.waw.pl>
2017-01-15 23:12:30 +0100

    4.

--- src/main/java/Login/controller/MailController.java
+++ src/main/java/Login/controller/MailController.java
@@ -2,11 +2,16 @@ package Login.controller;
 
 import Login.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.mail.*;
+import javax.mail.internet.MimeBodyPart;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.Properties;
 
 /**
@@ -18,15 +23,21 @@ public class MailController {
 
 
     //To jest wszystko do zmiany ale nie wiem jak to kurwa ogarnąć
-    String host = "pop.gmail.com";
-    String mailStoreType = "pop3";
-    String username = "zalogagpw@gmail.com";
-    String password = "loncjemaslo";
-    String port = "995";
-
 
     @RequestMapping("/gmail")
-    public void readEmail() throws MessagingException, IOException {
+    public ModelAndView getGetemailPage(@RequestParam Optional<String> error) {
+        return new ModelAndView("mail", "error", error);
+
+    }
+
+    @RequestMapping("/abc")
+    public ModelAndView readEmail(@RequestParam Optional<String> error,
+                                  @RequestParam String email,@RequestParam String password) throws MessagingException, IOException {
+
+
+        String host = "pop.gmail.com";
+        String mailStoreType = "pop3";
+        String port = "995";
         Properties properties = new Properties();
 
         properties.put("mail.pop3.host", host);
@@ -37,7 +48,7 @@ public class MailController {
         //create the POP3 store object and connect with the pop server
         Store store = emailSession.getStore("pop3s");
 
-        store.connect(host, username, password);
+        store.connect(host, email, password);
 
         //create the folder object and open it
         Folder emailFolder = store.getFolder("INBOX");
@@ -46,15 +57,32 @@ public class MailController {
         // retrieve the messages from the folder in an array and print it
         Message[] messages = emailFolder.getMessages();
         for (int i = 0, n = messages.length; i < n; i++) {
-            Message message = messages[i];
-            System.out.println("---------------------------------");
-            System.out.println("Email Number " + (i + 1));
-            System.out.println("Subject: " + message.getSubject());
-            System.out.println("From: " + message.getFrom()[0]);
-            System.out.println("Text: " + message.getContent().toString());
 
+            Message message = messages[i];
+            String contentType = message.getContentType();
+            if (contentType.contains("multipart")) {
+                Multipart multiPart = (Multipart) message.getContent();
+                int numberOfParts = multiPart.getCount();
+                for (int partCount = 0; partCount < numberOfParts; partCount++) {
+                    MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(partCount);
+                    if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
+
+                        System.out.println("---------------------------------");
+                        System.out.println("Email Number " + (i + 1));
+                        System.out.println("Subject: " + message.getSubject());
+                        System.out.println("From: " + message.getFrom()[0]);
+                        System.out.println("Text: " + message.getContent().toString());
+                        System.out.println("Att: "+part.getFileName());
+
+                    }
+                }
+            }
         }
         emailFolder.close(false);
         store.close();
+        return new ModelAndView("redirect:/");
     }
+
 }
+
+
